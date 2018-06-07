aab9d50f73b659328d6c4151487d1e79c5e89b85
Varadharajan <varadha@ymail.com>
2018-06-07 19:18:07 +0000

    Initial Apache Server cookbook commit

---
+++ recipes/server.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
