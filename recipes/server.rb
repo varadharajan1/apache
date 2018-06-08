123edd67e64c346195a87893aba7234f803108b7
Varadharajan <varadha@ymail.com>
2018-06-08 14:37:24 +0000

    tested with node object

--- recipes/server.rb
+++ recipes/server.rb
@@ -10,7 +10,10 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>IPADDRESS: #{node['ipaddress']}</h2>
+  "
 end
 
 service 'httpd' do
